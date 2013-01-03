9f463679194496e92fab6514c6be5d8b2e3e151c
Hassan <hassan.zaki@badrit.com>
2013-01-03 19:25:11 +0200

    init project

---
+++ emporium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emporium::Application.config.secret_token = '445aaefc3e0f2822baca27c0326675bd1679f727f4884fdf8075f5d12593f7c940b76630aef56ce34395492342d6aeb7af0ac0ecf9b072a0a96982f7a1d1c05f'
